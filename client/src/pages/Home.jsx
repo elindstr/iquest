f4caa86b7d5313ae49dee21597eed9cfc4fbe869
elindstr <elindstr@gmail.com>
2024-05-17 15:02:51 -0700

    added basic quiz functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import EgComponent from "../components/EgComponent";
+import { Link } from 'react-router-dom';
+import QuizSettings from '../components/quizSettings';
 
 const Home = () => {
   return (
-    <div className="container">
-      <EgComponent />
-    </div>
+    <>
+      <div className="container">
+        <h1>Welcome to iQuest</h1>
+
+        <Link to="/quiz">Start Quiz</Link>
+        
+        <br /> <br /> <br /> <br /> <hr />
+        <div>Hidden values we can control in background:</div>
+        <QuizSettings />
+        
+      </div>
+    </>
   );
 };
 
