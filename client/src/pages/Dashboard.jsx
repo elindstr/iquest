4e9ff0aa02edb402d414ceee723809d580defd67
elindstr <elindstr@gmail.com>
2024-05-23 11:29:25 -0700

    adding iq formula; refactored QuizBoard; added iq to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,8 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import Auth from '../utils/auth';
 import styles from './Dashboard.module.css';
 
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
+
 const Dashboard = () => {
   const navigate = useNavigate();
+  const userId = Auth.getProfile().data._id;
+  const { data: userData, loading, error } = useQuery(QUERY_USER, { variables: { _id: userId } });
 
   const handleLogout = () => {
     Auth.logout();
@@ -13,6 +19,7 @@ const Dashboard = () => {
   return (
     <div className={styles.dashboardPage}>
       <div className={styles.card}>
+        <span>{userData? userData.user.firstName: null}: {userData? userData.user.iq.toFixed(2): null} IQ</span>
         <h1 className={styles.h1}>Dashboard</h1>
         <button className={styles.button} onClick={() => navigate('/find-friends')}>Find Friends</button>
         <button className={styles.button} onClick={() => navigate('/update-profile')}>Update Profile</button>
