22b4bae1c33dfd36d1f072b70b76708411974c8f
GTodd711 <150283557+GTodd711@users.noreply.github.com>
2024-05-29 19:43:37 -0700

    merged code where needed removed duplicate exports/imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,9 +11,6 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-      },
       '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
